928ba0dbf915ae3889be9edc6d16008305faeab8
rohith203 <saranga.rohith@gmail.com>
2020-04-07 11:22:26 +0530

    add mysql functions

--- ChatBot/backend/chat.py
+++ ChatBot/backend/chat.py
@@ -1,12 +1,14 @@
 import os
 import aiml
 import sqlite3
+from db import populate_db, show_table, get_coursebydomain, get_profbycourse, get_coursebyfaculty
 
 
 BRAIN_FILE="brain.dump"
 # Create the kernel and learn AIML files
 
 kernel = aiml.Kernel()
+kernel.learn("basic_chat.aiml")
 # folder = 'aiml-en-us-foundation-alice-master'
 # for f in os.listdir(folder):
 #     kernel.learn(os.path.join(folder, f))
@@ -18,71 +20,40 @@ kernel = aiml.Kernel()
 #     kernel.bootstrap(learnFiles="basic_chat.aiml")
 #     print("Saving brain file: " + BRAIN_FILE)
 #     kernel.saveBrain(BRAIN_FILE)
-kernel.learn("basic_chat.aiml")
 
 # Press CTRL-C to break this loop
-# while True:
-#     print(kernel.respond(input("Enter your message >> ")))
-
 conn = sqlite3.connect("database.db")
-conn.execute('''CREATE TABLE IF NOT EXISTS FACULTY
-                (FID INT PRIMARY KEY     NOT NULL,
-                NAME           TEXT    NOT NULL,
-                DEPARTMENT     CHAR(50));''')
-conn.execute('''CREATE TABLE IF NOT EXISTS COURSES
-                (CID INT PRIMARY KEY     NOT NULL,
-                COURSE_NAME     TEXT    NOT NULL,
-                DEPARTMENT      char(50),
-                FID INT NOT NULL);''')
-conn.execute('''INSERT INTO FACULTY
-                (FID,    NAME,    DEPARTMENT) VALUES
-                (401,   "Jason	Blake", "CSE"),
-                (402,   "Phil	Morgan", "CSE"),
-                (403,   "Connor	Clarkson", "CSE"),
-                (404,   "Alan	King", "ECE"),
-                (405,   "Dominic	Greene", "ECE"),
-                (406,   "Ryan	Reid", "EEE"),
-                (407,   "Nicholas	Mitchell", "EEE"),
-                (408,   "Jonathan	Martin", "CIVIL"),
-                (409,   "Jake	Langdon", "CIVIL"),
-                (410,   "Carolyn	Harris", "MECH"),
-                (411,   "Andrea	Watson", "MECH"),
-                (412,   "Felicity	Dowd", "BIOT"),
-                (413,   "Natalie	Sharp", "BIOT"),
-                (414,   "Kylie	Edmunds", "MANU"),
-                (415,   "Julia	McLean", "CHEM");''')
-conn.execute('''INSERT INTO COURSES
-                (CID,  COURSE_NAME,                DEPARTMENT,  FID) VALUES
-                (101, "Foundation of Data Science"     , "CSE", 401),
-                (102, "Machine Learning"               , "CSE", 401),
-                (103, "Data Mining"                    , "CSE", 402),
-                (104, "Information Retrieval"          , "CSE", 401),
-                (105, "Software Engineering"           , "CSE", 402),
-                (106, "Computational Geometry"         , "CSE", 403),
-                (107, "Computer Graphics"              , "CSE", 403),
-                (108, "Cloud Computing"                , "CSE", 402),
-                (109, "Cryptography"                   , "CSE", 403),
-                (110, "Artificial Intelligence"        , "CSE", 403),
-                (116, "Introduction to Bioinformatics" , "BIOT",412),
-                (117, "Genomics"                       , "BIOT",413),
-                (118, "Introduction to Nanoscience"    , "CHEM",415),
-                (119, "Microfluidics and its application", "CHEM",415),
-                (120, "Computational Geomechanics"      , "CIVIL",408),
-                (121, "Introduction to Bridge Engineering", "CIVIL",409),
-                (122, "Communication Networks"         , "EEE", 406),
-                (123, "Digital Communication"          , "EEE", 406),
-                (124, "Satellite Communication"        , "ECE", 403),
-                (125, "Advanced Metal Forming"         , "MANU", 414),
-                (126, "Supply Chain Management"        , "MANU", 414),
-                (127, "Product Design"                 , "MECH", 410),
-                (128, "Power Plant Engineering"        , "MECH", 410);''')
+populate_db(conn)
+prev_domain = ""
+prev_faculty = ""
 
-                # (111, "Parallel Computing"             , "CSE"),
-                # (112, "Network Programming"            , "CSE"),
-                # (113, "Neural Networks and Fuzzy Logic", "CSE"),
-                # (114, "Cryptography"                   , "CSE"),
-                # (115, "Image Processing"               , "CSE"),
+while True:
+    domain = kernel.getPredicate('domain')
+    faculty = kernel.getPredicate('faculty')
+    if domain and prev_domain!=domain:
+        course = get_coursebydomain(conn, domain)
+        prof = get_profbycourse(conn, course)
+        print(f"You can take {course} course. {prof} is the faculty for this course.")
+        prev_domain = domain
+    elif faculty and prev_faculty!=faculty:
+        course = get_coursebyfaculty(conn, faculty)
+        if len(course)==0:
+            print("The faculty you mentioned is not available.")
+        else:
+            print(f"{faculty} teaches the course {course}. You should take it.")
+        prev_faculty = faculty
+    else:
+        print(kernel.respond(input("Enter your message >> ")))
+
+# Loading database
+# show_table(conn, "courses")
+# domain = "Software Engineering"
+# domain = "DATA"
+# course = get_coursebydomain(conn, domain)
+# get_profbycourse(conn, course)
 conn.close()
 
 def resp(inp):
-    return kernel.respond(inp)
+    # print()
+    res = kernel.respond(inp)
+    return res
---
+++ ChatBot/backend/db.py
@@ -0,0 +1,124 @@
+def populate_db(conn):
+    conn.execute('''DROP TABLE  IF EXISTS FACULTY;''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS FACULTY
+                (FID INT PRIMARY KEY     NOT NULL,
+                NAME           TEXT    NOT NULL,
+                DEPARTMENT     CHAR(50));''')
+    conn.execute('''CREATE TABLE IF NOT EXISTS COURSES
+                    (CID INT PRIMARY KEY     NOT NULL,
+                    COURSE_NAME     TEXT    NOT NULL,
+                    DEPARTMENT      char(50),
+                    DOMAIN     TEXT    NOT NULL,
+                    FID INT NOT NULL);''')
+    conn.execute('''INSERT INTO FACULTY
+                    (FID,    NAME,    DEPARTMENT) VALUES
+                    (401,   "Jason Blake", "CSE"),
+                    (402,   "Phil Morgan", "CSE"),
+                    (403,   "Connor Clarkson", "CSE"),
+                    (404,   "Alan King", "ECE"),
+                    (405,   "Dominic Greene", "ECE"),
+                    (406,   "Ryan Reid", "EEE"),
+                    (407,   "Nicholas Mitchell", "EEE"),
+                    (408,   "Jonathan Martin", "CIVIL"),
+                    (409,   "Jake Langdon", "CIVIL"),
+                    (410,   "Carolyn Harris", "MECH"),
+                    (411,   "Andrea Watson", "MECH"),
+                    (412,   "Felicity Dowd", "BIOT"),
+                    (413,   "Natalie Sharp", "BIOT"),
+                    (414,   "Kylie Edmunds", "MANU"),
+                    (415,   "Julia McLean", "CHEM");''')
+    conn.execute('''INSERT INTO COURSES
+                    (CID,  COURSE_NAME,                DEPARTMENT, DOMAIN,  FID) VALUES
+                    (101, "Foundation of Data Science"     , "CSE", "Data Science" ,401),
+                    (102, "Machine Learning"               , "CSE", "Data Science"  ,401),
+                    (103, "Data Mining"                    , "CSE", "Data Science"  ,402),
+                    (104, "Information Retrieval"          , "CSE", "Data Science"  ,401),
+                    (105, "Software Engineering"           , "CSE", "Software Eng"  ,402),
+                    (106, "Computational Geometry"         , "CSE", "Math" ,403),
+                    (107, "Computer Graphics"              , "CSE", "Image processing",403),
+                    (108, "Cloud Computing"                , "CSE", "Cloud Technology",402),
+                    (109, "Cryptography"                   , "CSE", "Computer Security",403),
+                    (110, "Artificial Intelligence"        , "CSE", "Data Science",403),
+                    (116, "Introduction to Bioinformatics" ,"BIOT", "Bio Technology" ,412),
+                    (117, "Genomics"                       ,"BIOT", "Bio Technology" ,413),
+                    (118, "Introduction to Nanoscience"    ,"CHEM", "Chemistry" ,415),
+                    (119, "Microfluidics and its application","CHEM", "Chemistry" ,415),
+                    (120, "Computational Geomechanics"      , "CIVIL", "Civil" ,408),
+                    (121, "Introduction to Bridge Engineering", "CIVIL", "Civil" ,409),
+                    (122, "Communication Networks"         , "EEE", "Communications" , 406),
+                    (123, "Digital Communication"          , "EEE",  "Communications" ,406),
+                    (124, "Satellite Communication"        , "ECE", "Communications" , 403),
+                    (125, "Advanced Metal Forming"         , "MANU", "Manufacturing" , 414),
+                    (126, "Supply Chain Management"        , "MANU", "Manufacturing" , 414),
+                    (127, "Product Design"                 , "MECH", "Mechanical" , 410),
+                    (128, "Power Plant Engineering"        , "MECH", "Mechanical" ,410);''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS TRENDING
+                (RANK INT PRIMARY KEY     NOT NULL,
+                DOMAIN           TEXT    NOT NULL);''')
+    conn.execute('''INSERT INTO TRENDING
+                (RANK, DOMAIN) VALUES
+                (1, "Data Science"),
+                (2, "Cloud Technology"),
+                (3, "Software Eng"),
+                (4, "Computer Security"),
+                (5, "Communications"),
+                (6, "Image Processing"),
+                (7, "Bio Technology"),
+                (8, "Chemistry"),
+                (9, "Math"),
+                (10, "Civil"),
+                (11, "Manufacturing"),
+                (12, "Mechanical");''')
+
+
+def show_table(conn, table_name):
+    curr = conn.cursor()
+    curr.execute(f"SELECT * FROM {table_name};")
+    tab = curr.fetchall()
+    for row in tab:
+        print(row)
+
+def get_coursebydomain(conn, domain):
+    curr = conn.cursor()
+    curr.execute(f"SELECT DOMAIN FROM TRENDING;")
+    tab = curr.fetchall()
+    for row in tab:
+        if (domain.lower() in row[0].lower()) or (row[0].lower() in domain.lower()):
+            domain = row[0].lower()
+
+    course = ""
+    curr = conn.cursor()
+    curr.execute(f"SELECT COURSE_NAME FROM COURSES, TRENDING WHERE LOWER(TRENDING.DOMAIN) like '{domain.lower()}' AND COURSES.DOMAIN=TRENDING.DOMAIN;")
+    tab = curr.fetchall()
+    if len(tab)>0:
+        course = tab[0][0]
+    # print(course)
+    return course
+
+def get_profbycourse(conn, course):
+    curr = conn.cursor()
+    curr.execute(f"SELECT NAME FROM FACULTY, COURSES WHERE FACULTY.FID=COURSES.FID AND COURSES.COURSE_NAME='{course}';")
+    tab = curr.fetchall()
+    if len(tab)>0:
+        prof = tab[0][0]
+    # print(prof)
+    return prof
+
+def get_coursebyfaculty(conn, faculty):
+    curr = conn.cursor()
+    curr.execute(f"SELECT NAME FROM FACULTY;")
+    tab = curr.fetchall()
+    for row in tab:
+        if (faculty.lower() in row[0].lower()) or (row[0].lower() in faculty.lower()):
+            faculty = row[0]
+
+    course = ""
+    curr = conn.cursor()
+    curr.execute(f"SELECT COURSE_NAME FROM FACULTY, COURSES WHERE FACULTY.FID=COURSES.FID AND FACULTY.NAME='{faculty}';")
+    tab = curr.fetchall()
+    if len(tab)>0:
+        course = tab[0][0]
+    # print(prof)
+    return course
