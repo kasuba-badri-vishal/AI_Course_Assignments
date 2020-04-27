227675c00d9fc4d9b72d07cfed30333c22918258
rohith203 <saranga.rohith@gmail.com>
2020-04-27 19:06:36 +0530

    add apk in chatbot, and modified a2/q2

---
+++ assign1/ChatBot/readme.md
@@ -0,0 +1,19 @@
+# AIML - Chatbot
+
+- A chatbot has been created using aiml. The bot's core is present in a flask server. 
+- It uses sqlite database for faculty, courses information.
+- The user can ask the bot for help in choosing electives
+
+- The UI for chatbot has been designed in android. The apk file is present in this folder.
+
+<img src="./example conversation1 UI.jpeg" height="400px" >
+
+### To run the flask server
+<code>python server.py</code>
+
+- The server has also been deployed in heroku at https://aiml-chatbot.herokuapp.com
+
+- In the app the user can either use this website or choose to 
+connect to another server running on a different url.
+
+<img src="./homepage.jpeg" height="300px" >
--- assign2/q2/main.py
+++ assign2/q2/main.py
@@ -1,6 +1,7 @@
 from pyke import knowledge_base, knowledge_engine, ask_tty
 import pandas as pd
 from create_rule_file import create_bc_rules
+import shutil
 
 RULE_FILE = "bc_rules"
 
@@ -11,8 +12,10 @@ def check_disease(disease, i):
         vals, plans = my_engine.prove_1_goal(f"{RULE_FILE}.has_disease{i}()")
         print(f"\nDiagnosis: You could have {disease}.")
         print(f"\nTREATMENT: {df.iloc[i, 3]}\n")
+        print(f'''Rule used: IF {" ^ ".join(df.iloc[i,2].split(';'))} THEN {disease}''')
+
         return vals, plans
-    except:
+    except knowledge_engine.CanNotProve:
         if i+1<df.shape[0]:
             check_disease(df.iloc[i+1, 0], i+1)
         else:
@@ -21,12 +24,25 @@ def check_disease(disease, i):
 if __name__=="__main__":
     df = pd.read_csv('./trimmed_dataset.csv', encoding='utf-8')
     RULE_FILE = create_bc_rules(df)
+    try:
+        shutil.rmtree('./compiled_krb')
+    except:
+        pass
     
     my_engine = knowledge_engine.engine('./knowledgebase')
     my_engine.ask_module = ask_tty
     my_engine.reset()
 
+    print('''\n\n*****Welcome to Medical Diagnosis System******
+    I am your personal Diagnosis Expert System. Please answer the following questions''')
 
+    my_engine.activate('ask_rules')
     my_engine.activate(RULE_FILE)
+
+    vals, plans = my_engine.prove_1_goal(f"ask_rules.get_name($name)")
+    print(vals['name'])
+    
+    # vals, plans = my_engine.prove_1_goal(f"ask_rules.get_age($age)")
+    # print(vals['age'])
 
     check_disease(df.iloc[0,0],0)
